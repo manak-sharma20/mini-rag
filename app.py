74747e70892f762e3c1f5c4849db66e5fa3d5ba8
Kumar manak <manakkash20@gmail.com>
2026-02-08 18:06:50 +0530

    fix: prevent cloud crash by loading stored embeddings instead of rebuilding model

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import FakeEmbeddings
 
 
 DB_PATH = "db"
@@ -11,8 +12,12 @@ question = st.text_input("Ask a question about the PDF")
 
 if question:
 
-    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-    db = Chroma(persist_directory=DB_PATH, embedding_function=embeddings)
+    embeddings = FakeEmbeddings(size=384)
+
+    db = Chroma(
+    persist_directory=DB_PATH,
+    embedding_function=embeddings
+)
 
     # retrieve relevant chunks
     docs = db.similarity_search(question, k=4)
